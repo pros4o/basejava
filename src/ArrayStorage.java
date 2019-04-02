07dff6dfbb23e35927b31023edbc08491abc640c
pros4o <Easy8r99ze@yandex.ru>
2019-04-03 01:54:58 +0300

    HW1. Attempt #1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,34 +1,80 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
+    private int elements = 0;
+    /**
+     * Обнуление массива
+     */
     void clear() {
-    }
-
-    void save(Resume r) {
-    }
-
-    Resume get(String uuid) {
-        return null;
+        Arrays.fill(storage,null);
+        elements = 0;
     }
 
     /**
-     * тест
-     * @param uuid
+     * Метод для сохранения резюме
+     * @param r - элемент для сохранения
+     */
+    void save(Resume r) {
+        for(int cariage = 0; cariage < storage.length - 1; cariage++) {
+            if (storage[cariage] == null) {
+                storage[cariage] = r;
+                elements++;
+                break;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param uuid - метка резюме
+     * @return резюме по его uuid
+     */
+    Resume get(String uuid) {
+        Resume temp = new Resume();
+        temp.uuid = uuid;
+        for (int i=0; i<elements - 1;i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                temp = storage[i];
+            }
+        }
+        System.out.println("Такого элемента нету!");
+        return temp;
+    }
+
+    /**
+     * Удаляет элемент из массива по его uuid
+     * @param uuid - метка резюме
      */
     void delete(String uuid) {
+        int index = 0;
+
+        for (int i = 0; i < elements - 1; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+            }
+        }
+        if (index == elements) {
+            storage[index] = null;
+        } else {
+            for (int i = index; index + i <= elements; i++)
+                storage[i] = storage[i + 1]; //until index +1 <= elements;
+        }
+        elements--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, elements);
     }
 
     int size() {
-        return 0;
+        return elements;
     }
 }
