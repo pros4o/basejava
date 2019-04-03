1669de4829c05277e5e45b3f7fbd1c28e987a86f
pros4o <Easy8r99ze@yandex.ru>
2019-04-03 23:40:33 +0300

    HW1. Attempt #2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,70 +1,58 @@
-import java.lang.reflect.Array;
+//import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
     private int elements = 0;
+
     /**
      * Обнуление массива
      */
     void clear() {
-        Arrays.fill(storage,null);
+        //Arrays.fill(storage,null);
+        Arrays.fill(storage, 0, elements, null);
         elements = 0;
     }
 
     /**
      * Метод для сохранения резюме
+     *
      * @param r - элемент для сохранения
      */
     void save(Resume r) {
-        for(int cariage = 0; cariage < storage.length - 1; cariage++) {
-            if (storage[cariage] == null) {
-                storage[cariage] = r;
-                elements++;
-                break;
-            }
-        }
+        storage[elements] = r;
+        elements++;
     }
 
     /**
-     *
      * @param uuid - метка резюме
      * @return резюме по его uuid
      */
     Resume get(String uuid) {
-        Resume temp = new Resume();
-        temp.uuid = uuid;
-        for (int i=0; i<elements - 1;i++) {
-            if(storage[i].uuid.equals(uuid)) {
-                temp = storage[i];
+        for (int i = 0; i < elements; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
-        System.out.println("Такого элемента нету!");
-        return temp;
+        return null;
     }
 
     /**
      * Удаляет элемент из массива по его uuid
+     *
      * @param uuid - метка резюме
      */
     void delete(String uuid) {
-        int index = 0;
-
-        for (int i = 0; i < elements - 1; i++) {
+        for (int i = 0; i < elements; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                index = i;
+                storage[i] = storage[elements - 1];
+                storage[elements - 1] = null;
+                elements--;
             }
         }
-        if (index == elements) {
-            storage[index] = null;
-        } else {
-            for (int i = index; index + i <= elements; i++)
-                storage[i] = storage[i + 1]; //until index +1 <= elements;
-        }
-        elements--;
     }
 
     /**
