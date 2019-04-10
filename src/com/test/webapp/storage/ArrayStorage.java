8b7839c138a4674c277ab0a7981b596340b13ba3
pros4o <Easy8r99ze@yandex.ru>
2019-04-11 02:55:02 +0300

    HW2. Добавил: 1) Поиск индекса 2) Проверка на наличие резюме 3) немного убрал дублирование

--- src/com/test/webapp/storage/ArrayStorage.java
+++ src/com/test/webapp/storage/ArrayStorage.java
@@ -1,5 +1,6 @@
 package com.test.webapp.storage;
-
+//!! не забыть отформотировать его
+//
 import java.util.Arrays;
 import com.test.webapp.model.Resume;
 
@@ -7,8 +8,9 @@ import com.test.webapp.model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
-    private int elements = 0;
+    private final static int MAX_SIGE = 2;//не забыть вернуть на 10.000//Максимальное количество резюме
+    private Resume[] storage = new Resume[MAX_SIGE];
+    private int elements = 0; //количество резюме в хранилке
 
     /**
      * Обнуление массива
@@ -24,6 +26,10 @@ public class ArrayStorage {
      * @param r - элемент для сохранения
      */
     public void save(Resume r) {
+        if((elements + 1) > MAX_SIGE) {
+            System.out.println("Attention. Overflow");
+            return;
+        }
         for (int i = 0; i < elements; i++) {
             if (storage[i].getUuid().equals(r.getUuid())) {
                 System.out.println("Элемент уже существует");
@@ -39,13 +45,14 @@ public class ArrayStorage {
      * @return резюме по его uuid
      */
     public Resume get(String uuid) {
-        for (int i = 0; i < elements; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
+        if (!checkResume(uuid)) {
+            System.out.println("Resume ... " + uuid + " not found");
+            return null;
+        } else {
+            return storage[getIndex(uuid)];
         }
-        return null;
     }
+
 
     /**
      * Удаляет элемент из массива по его uuid
@@ -63,13 +70,53 @@ public class ArrayStorage {
     }
 
     /**
+     * Вывод весь массив
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, elements);
     }
 
+    /**
+     * @return number of resume
+     */
     public int size() {
         return elements;
+    }
+
+    /**
+     * Проверка на наличие резюме
+     * @param r
+     * @return
+     */
+    private boolean checkResume(Resume r) {
+        boolean flag = false;
+        if (getIndex(r.getUuid()) >= 0) flag = true;
+        return flag;
+    }
+
+    /**
+     * Проверка на наличие резюме
+     * @param uuid
+     * @return
+     */
+    private boolean checkResume(String uuid) {
+        boolean flag = false;
+        if (getIndex(uuid) >= 0) flag = true;
+        return flag;
+    }
+
+    /**
+     * В поисках индекса.
+     * @param uuid
+     * @return
+     */
+    private int getIndex(String uuid) {
+        for (int i = 0; i < elements; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
