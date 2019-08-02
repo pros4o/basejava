1747770912933dca22efec90d3e982e3df0cb1fb
pros4o <Easy8r99ze@yandex.ru>
2019-08-02 23:55:30 +0300

    HW11 make deadlock

---
+++ src/com/test/webapp/MainDeadLock.java
@@ -0,0 +1,61 @@
+package com.test.webapp;
+
+public class MainDeadLock {
+
+    private static int count = 1_000_000;
+    private static int result = 1;
+
+    private static final Object LOCK1 = new Object();
+    private static final Object LOCK2 = new Object();
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        MainDeadLock.dev();
+                        System.out.println(getName() + " , " + getState());
+                        System.out.println(count);
+                    }
+                }.start();
+            } else {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        MainDeadLock.inc();
+                        System.out.println(getName() + " , " + getState());
+                        System.out.println(count);
+                    }
+                }.start();
+            }
+        }
+
+    }
+
+    private static void inc() {
+        synchronized(LOCK2) {
+            count++;
+            for (int factor = 2; factor <= count; factor++) {
+                result *= factor;
+                synchronized (LOCK1) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    private static void dev() {
+        synchronized (LOCK1) {
+            count--;
+            for (int factor = 2; factor <= count; factor++) {
+                result *= factor;
+                synchronized (LOCK2) {
+                    count--;
+                }
+            }
+        }
+
+    }
+}
